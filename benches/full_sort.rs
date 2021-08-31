fb386b37b31ce03aec0c53a184adbfac98ef8ce7
Nathan Essex <nathan@essex.id.au>
2021-08-31 21:39:52 +0900

    Add worst-case bimodal dataset benchmark

--- benches/full_sort.rs
+++ benches/full_sort.rs
@@ -188,6 +188,71 @@ fn full_sort_u64_truncated(c: &mut Criterion) {
     group.finish();
 }
 
+fn full_sort_u64_bimodal(c: &mut Criterion) {
+    let n = 200_000_000;
+    let mut inputs = Vec::with_capacity(n);
+    let mut rng = WyRand::new();
+    let shift = 32u64;
+
+    for _ in 0..(n/2) {
+        inputs.push(rng.generate::<u64>() >> shift);
+    }
+
+    for _ in 0..(n/2) {
+        inputs.push(rng.generate::<u64>() << shift);
+    }
+
+    let input_sets: Vec<Vec<u64>> = vec![
+        inputs.clone(),
+        inputs[..100_000_000].to_vec(),
+        inputs[..50_000_000].to_vec(),
+        inputs[..10_000_000].to_vec(),
+        inputs[..5_000_000].to_vec(),
+        inputs[..2_000_000].to_vec(),
+        inputs[..1_000_000].to_vec(),
+        inputs[..500_000].to_vec(),
+        inputs[..300_000].to_vec(),
+        inputs[..200_000].to_vec(),
+        inputs[..100_000].to_vec(),
+        inputs[..50_000].to_vec(),
+        inputs[..10_000].to_vec(),
+        inputs[..5_000].to_vec(),
+    ];
+
+    drop(inputs);
+
+    let mut group = c.benchmark_group("full_sort_u64_bimodal");
+    group.sample_size(10);
+    group.measurement_time(Duration::from_secs(5));
+    group.warm_up_time(Duration::from_secs(1));
+    for set in input_sets.iter() {
+        let l = set.len();
+        group.throughput(Throughput::Elements(l as u64));
+        group.bench_with_input(BenchmarkId::new("rdst", l), set, |bench, set| {
+            bench.iter_batched(
+                || set.clone(),
+                |mut input| {
+                    input.radix_sort_unstable();
+                    black_box(input);
+                },
+                BatchSize::SmallInput,
+            );
+        });
+
+        group.bench_with_input(BenchmarkId::new("voracious", l), set, |bench, set| {
+            bench.iter_batched(
+                || set.clone(),
+                |mut input| {
+                    input.voracious_mt_sort(num_cpus::get());
+                    black_box(input);
+                },
+                BatchSize::SmallInput,
+            );
+        });
+    }
+    group.finish();
+}
+
 fn full_sort_u64_truncated_shifted(c: &mut Criterion) {
     let n = 200_000_000;
     let mut inputs = Vec::with_capacity(n);
@@ -248,5 +313,5 @@ fn full_sort_u64_truncated_shifted(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, full_sort_u32, full_sort_u64, full_sort_u64_truncated, full_sort_u64_truncated_shifted);
+criterion_group!(benches, full_sort_u32, full_sort_u64, full_sort_u64_truncated, full_sort_u64_bimodal, full_sort_u64_truncated_shifted);
 criterion_main!(benches);
