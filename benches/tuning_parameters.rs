56fbb818970efa0d13a644fc7e37bf50b5cfb89f
Nathan Essex <nathan@essex.id.au>
2021-09-07 03:58:21 +0900

    Refactor benchmarks for tuning parameters

--- benches/tuning_parameters.rs
+++ benches/tuning_parameters.rs
@@ -1,251 +1,137 @@
 use criterion::*;
-use nanorand::{Rng, WyRand};
+use nanorand::{WyRand, RandomGen};
 use rdst::utils::*;
-use rdst::sorts::ska_sort::ska_sort;
+use rdst::sorts::ska_sort::ska_sort_adapter;
 use rdst::sorts::scanning_radix_sort::scanning_radix_sort;
 use rdst::sorts::lsb_radix_sort::lsb_radix_sort_adapter;
 use rdst::tuning_parameters::TuningParameters;
 use std::time::Duration;
+use rdst::RadixKey;
+use rdst::test_utils::gen_bench_input_set;
+use std::fmt::Debug;
+use std::ops::{Shl, Shr};
+use rdst::sorts::recombinating_sort::recombinating_sort;
 
-fn counts(c: &mut Criterion) {
-    let n = 500_000_000;
-    let mut inputs = Vec::with_capacity(n);
-    let mut rng = WyRand::new();
+fn bench_common<T>(
+    c: &mut Criterion,
+    shift: T,
+    group: &str,
+    tests: Vec<(&str, Box<dyn Fn(Vec<T>)>)>,
+)
+where
+    T: RadixKey
+        + Ord
+        + RandomGen<WyRand>
+        + Clone
+        + Debug
+        + Send
+        + Sized
+        + Copy
+        + Sync
+        + Shl<Output = T>
+        + Shr<Output = T>,
+{
+    let input_sets = gen_bench_input_set(shift);
 
-    for _ in 0..n {
-        inputs.push(rng.generate::<u32>());
-    }
-
-    let input_sets: Vec<Vec<u32>> = vec![
-        inputs.clone(),
-        inputs[..200_000_000].to_vec(),
-        inputs[..100_000_000].to_vec(),
-        inputs[..50_000_000].to_vec(),
-        inputs[..10_000_000].to_vec(),
-        inputs[..5_000_000].to_vec(),
-        inputs[..2_000_000].to_vec(),
-        inputs[..1_000_000].to_vec(),
-        inputs[..500_000].to_vec(),
-        inputs[..450_000].to_vec(),
-        inputs[..400_000].to_vec(),
-        inputs[..350_000].to_vec(),
-        inputs[..300_000].to_vec(),
-        inputs[..200_000].to_vec(),
-        inputs[..100_000].to_vec(),
-        inputs[..50_000].to_vec(),
-        inputs[..10_000].to_vec(),
-        inputs[..5_000].to_vec(),
-    ];
-
-    drop(inputs);
-
-    let mut group = c.benchmark_group("counts");
+    let mut group = c.benchmark_group(group);
     group.sample_size(10);
     group.measurement_time(Duration::from_secs(5));
     group.warm_up_time(Duration::from_secs(1));
+
     for set in input_sets.iter() {
         let l = set.len();
         group.throughput(Throughput::Elements(l as u64));
-        group.bench_with_input(BenchmarkId::new("get_counts", l), set, |bench, set| {
-            bench.iter_batched(
-                || set.clone(),
-                |input| {
-                    let c = get_counts(&input, 0);
-                    black_box(c);
-                },
-                BatchSize::SmallInput,
-            );
-        });
 
-        group.bench_with_input(BenchmarkId::new("par_get_counts", l), set, |bench, set| {
-            bench.iter_batched(
-                || set.clone(),
-                |input| {
-                    let c = par_get_counts(&input, 0);
-                    black_box(c);
-                },
-                BatchSize::SmallInput,
-            );
-        });
+        for t in tests.iter() {
+
+            group.bench_with_input(BenchmarkId::new((*t).0.clone(), l), set, |bench, set| {
+                bench.iter_batched(|| set.clone(), &*t.1,BatchSize::SmallInput);
+            });
+        }
     }
     group.finish();
 }
 
-fn scanning_sort(c: &mut Criterion) {
-    let n = 200_000_000;
-    let mut inputs = Vec::with_capacity(n);
-    let mut rng = WyRand::new();
-    let tuning = TuningParameters::new(4);
-
-    for _ in 0..n {
-        inputs.push(rng.generate::<u32>());
-    }
-
-    let input_sets: Vec<Vec<u32>> = vec![
-        inputs.clone(),
-        inputs[..100_000_000].to_vec(),
-        inputs[..50_000_000].to_vec(),
-        inputs[..10_000_000].to_vec(),
-        inputs[..5_000_000].to_vec(),
-        inputs[..2_000_000].to_vec(),
-        inputs[..1_000_000].to_vec(),
-        inputs[..500_000].to_vec(),
-        inputs[..300_000].to_vec(),
-        inputs[..200_000].to_vec(),
-        inputs[..100_000].to_vec(),
-        inputs[..50_000].to_vec(),
-        inputs[..10_000].to_vec(),
-        inputs[..5_000].to_vec(),
+fn counts(c: &mut Criterion)
+{
+    let tests: Vec<(&str, Box<dyn Fn(Vec<_>)>)> = vec![
+        ("get_counts", Box::new(|input: Vec<_>| {
+            let c = get_counts(&input, 0);
+            black_box(c);
+        })),
+        ("par_get_counts", Box::new(|input: Vec<_>| {
+            let c = par_get_counts(&input, 0);
+            black_box(c);
+        })),
     ];
 
-    drop(inputs);
-
-    let mut group = c.benchmark_group("scanning_sort_level_4");
-    group.sample_size(10);
-    group.measurement_time(Duration::from_secs(5));
-    group.warm_up_time(Duration::from_secs(1));
-    for set in input_sets.iter() {
-        let l = set.len();
-        group.throughput(Throughput::Elements(l as u64));
-        group.bench_with_input(
-            BenchmarkId::new("scanning_radix_sort", l),
-            set,
-            |bench, set| {
-                bench.iter_batched(
-                    || set.clone(),
-                    |mut input| {
-                        scanning_radix_sort(&tuning, &mut input, 3, false);
-                        black_box(input);
-                    },
-                    BatchSize::SmallInput,
-                );
-            },
-        );
-
-        group.bench_with_input(BenchmarkId::new("lsb_radix_sort", l), set, |bench, set| {
-            bench.iter_batched(
-                || set.clone(),
-                |mut input| {
-                    lsb_radix_sort_adapter(&mut input, 0, 3);
-                    black_box(input);
-                },
-                BatchSize::SmallInput,
-            );
-        });
-    }
-    group.finish();
+    bench_common::<>(
+        c,
+        0u32,
+        "counts",
+        tests,
+    );
 }
 
-fn bench_ska_sort(c: &mut Criterion) {
-    let n = 10_000_000;
-    let mut inputs = Vec::with_capacity(n);
-    let mut rng = WyRand::new();
-    let tuning = TuningParameters::new(8);
-
-    for _ in 0..n {
-        inputs.push(rng.generate::<u32>());
-    }
-
-    let input_sets: Vec<Vec<u32>> = vec![
-        inputs.clone(),
-        inputs[..5_000_000].to_vec(),
-        inputs[..2_000_000].to_vec(),
-        inputs[..1_000_000].to_vec(),
-        inputs[..500_000].to_vec(),
-        inputs[..300_000].to_vec(),
-        inputs[..200_000].to_vec(),
-        inputs[..100_000].to_vec(),
-        inputs[..50_000].to_vec(),
-        inputs[..10_000].to_vec(),
-        inputs[..5_000].to_vec(),
+fn all_sort_common<T>(c: &mut Criterion, shift: T, name_suffix: &str)
+where
+    T: RadixKey
+        + Ord
+        + RandomGen<WyRand>
+        + Clone
+        + Debug
+        + Send
+        + Sized
+        + Copy
+        + Sync
+        + Shl<Output = T>
+        + Shr<Output = T>,
+{
+    let tests: Vec<(&str, Box<dyn Fn(Vec<_>)>)> = vec![
+        ("scanning_radix_sort", Box::new(|mut input| {
+            let tuning = TuningParameters::new(4);
+            scanning_radix_sort(&tuning, &mut input, 3, false);
+            black_box(input);
+        })),
+        ("lsb_radix_sort", Box::new(|mut input| {
+            lsb_radix_sort_adapter(&mut input, 0, 3);
+            black_box(input);
+        })),
+        ("ska_sort", Box::new(|mut input| {
+            let tuning = TuningParameters::new(4);
+            ska_sort_adapter(&tuning, &mut input, 3);
+            black_box(input);
+        })),
+        ("recombinating_sort", Box::new(|mut input| {
+            let tuning = TuningParameters::new(4);
+            recombinating_sort(&tuning, &mut input, 3);
+            black_box(input);
+        })),
     ];
 
-    drop(inputs);
-
-    let mut group = c.benchmark_group("ska_sort_level_4");
-    group.sample_size(10);
-    group.measurement_time(Duration::from_secs(5));
-    group.warm_up_time(Duration::from_secs(1));
-    for set in input_sets.iter() {
-        let l = set.len();
-        group.throughput(Throughput::Elements(l as u64));
-        group.bench_with_input(BenchmarkId::new("ska_sort", l), set, |bench, set| {
-            bench.iter_batched(
-                || set.clone(),
-                |mut input| {
-                    //ska_sort(&tuning, &mut input, 3, false);
-                    black_box(input);
-                },
-                BatchSize::SmallInput,
-            );
-        });
-
-        group.bench_with_input(BenchmarkId::new("lsb_radix_sort", l), set, |bench, set| {
-            bench.iter_batched(
-                || set.clone(),
-                |mut input| {
-                    lsb_radix_sort_adapter(&mut input, 0, 3);
-                    black_box(input);
-                },
-                BatchSize::SmallInput,
-            );
-        });
-    }
-    group.finish();
-
-    let mut inputs = Vec::with_capacity(n);
-
-    for _ in 0..n {
-        inputs.push(rng.generate::<u64>());
-    }
-
-    let input_sets: Vec<Vec<u64>> = vec![
-        inputs.clone(),
-        inputs[..5_000_000].to_vec(),
-        inputs[..2_000_000].to_vec(),
-        inputs[..1_000_000].to_vec(),
-        inputs[..500_000].to_vec(),
-        inputs[..300_000].to_vec(),
-        inputs[..200_000].to_vec(),
-        inputs[..100_000].to_vec(),
-        inputs[..50_000].to_vec(),
-        inputs[..10_000].to_vec(),
-        inputs[..5_000].to_vec(),
-    ];
-
-    drop(inputs);
-
-    let mut group = c.benchmark_group("ska_sort_level_8");
-    group.sample_size(10);
-    group.measurement_time(Duration::from_secs(5));
-    group.warm_up_time(Duration::from_secs(1));
-    for set in input_sets.iter() {
-        let l = set.len();
-        group.throughput(Throughput::Elements(l as u64));
-        group.bench_with_input(BenchmarkId::new("ska_sort", l), set, |bench, set| {
-            bench.iter_batched(
-                || set.clone(),
-                |mut input| {
-                    //ska_sort(&tuning, &mut input, 3, false);
-                    black_box(input);
-                },
-                BatchSize::SmallInput,
-            );
-        });
-
-        group.bench_with_input(BenchmarkId::new("lsb_radix_sort", l), set, |bench, set| {
-            bench.iter_batched(
-                || set.clone(),
-                |mut input| {
-                    lsb_radix_sort_adapter(&mut input, 0, 3);
-                    black_box(input);
-                },
-                BatchSize::SmallInput,
-            );
-        });
-    }
-    group.finish();
+    bench_common::<>(
+        c,
+        shift,
+        &("all_sort_".to_owned() + name_suffix),
+        tests,
+    );
 }
 
-criterion_group!(tuning_parameters, counts, scanning_sort, bench_ska_sort);
+fn all_sort_u32(c: &mut Criterion) {
+    all_sort_common(c, 0u32, "u32");
+}
+
+fn all_sort_u64(c: &mut Criterion) {
+    all_sort_common(c, 0u64, "u64");
+}
+
+fn all_sort_u32_bimodal(c: &mut Criterion) {
+    all_sort_common(c, 16u32, "u32_bimodal");
+}
+
+fn all_sort_u64_bimodal(c: &mut Criterion) {
+    all_sort_common(c, 32u64, "u64_bimodal");
+}
+
+criterion_group!(tuning_parameters, counts, all_sort_u32, all_sort_u64, all_sort_u32_bimodal, all_sort_u64_bimodal);
 criterion_main!(tuning_parameters);
