9b64f59e40cc9feb2b17884a1ddc08640108ad50
Nathan Essex <nathan@essex.id.au>
2021-11-18 01:01:02 +0900

    Try combining the counting phase with the previous write phase

--- src/sorts/mt_lsb_sort.rs
+++ src/sorts/mt_lsb_sort.rs
@@ -1,5 +1,4 @@
 use arbitrary_chunks::ArbitraryChunks;
-use rayon::current_num_threads;
 use rayon::prelude::*;
 use crate::utils::*;
 use crate::RadixKey;
@@ -10,105 +9,177 @@ pub const fn cdiv(a: usize, b: usize) -> usize {
     (a + b - 1) / b
 }
 
-pub fn mt_lsb_sort<T>(src_bucket: &mut [T], dst_bucket: &mut [T], level: usize)
-where
-    T: RadixKey + Sized + Send + Copy + Sync,
-{
-    if src_bucket.len() <= 1 {
-        return;
-    }
-
-    let threads = current_num_threads() * 8;
-    let bucket_len = src_bucket.len();
-    let chunk_size = (bucket_len / threads) + 1;
-
-    let locals: Vec<[usize; 256]> = src_bucket
-        .par_chunks(chunk_size)
-        .map(|chunk| get_counts(chunk, level))
-        .collect();
-
-    let tiles = locals.len();
-    let mut minor_counts = Vec::with_capacity(256 * tiles);
+fn counts_to_minor_counts(counts: Vec<[usize; 256]>) -> Vec<usize> {
+    let mut minor_counts = Vec::with_capacity(256 * counts.len());
 
     for b in 0..256 {
-        for c in 0..tiles {
-            minor_counts.push(locals[c][b]);
+        for c in 0..counts.len() {
+            minor_counts.push(counts[c][b]);
         }
     }
 
-    let mut chunks: Vec<&mut [T]> = dst_bucket.arbitrary_chunks_mut(minor_counts).collect();
+    minor_counts
+}
+
+fn get_minor_counts<T>(tile_size: usize, src_bucket: &mut [T], level: usize) -> Vec<usize>
+where
+    T: RadixKey + Sized + Send + Copy + Sync,
+{
+    let locals: Vec<[usize; 256]> = src_bucket
+        .par_chunks(tile_size)
+        .map(|chunk| get_counts(chunk, level))
+        .collect();
+
+    counts_to_minor_counts(locals)
+}
+
+pub fn chunk_and_collate<T>(dst_bucket: &mut [T], minor_counts: Vec<usize>, num_tiles: usize) -> Vec<Vec<(usize, &mut [T])>>
+{
+    let chunks: Vec<&mut [T]> = dst_bucket.arbitrary_chunks_mut(minor_counts).collect();
+
+    let mut running_total = 0;
+    let mut chunks: Vec<(usize, &mut [T])> = chunks.into_iter().map(|c| {
+        let t = running_total;
+        running_total += c.len();
+
+        (t, c)
+    }).collect();
+
     chunks.reverse();
 
-    let mut collated_chunks: Vec<Vec<&mut [T]>> = Vec::with_capacity(tiles);
-    collated_chunks.resize_with(tiles, || Vec::new());
+    let mut collated_chunks: Vec<Vec<(usize, &mut [T])>> = Vec::with_capacity(num_tiles);
+    collated_chunks.resize_with(num_tiles, || Vec::with_capacity(256));
 
     for _ in 0..256 {
-        for t in 0..tiles {
+        for t in 0..num_tiles {
             collated_chunks[t].push(chunks.pop().unwrap());
         }
     }
 
     collated_chunks
+}
+
+pub fn write_and_count<T>(
+    src_bucket: &mut [T],
+    dst_chunks: Vec<Vec<(usize, &mut [T])>>,
+    level: usize,
+    tile_exponent: usize,
+    tile_size: usize,
+    num_tiles: usize,
+) -> Vec<usize>
+where
+    T: RadixKey + Sized + Send + Copy + Sync,
+{
+    let next_level = level + 1;
+    let mut all_counts: Vec<Vec<[usize; 256]>> = dst_chunks
         .into_par_iter()
-        .zip(src_bucket.par_chunks(chunk_size))
+        .zip(src_bucket.par_chunks(tile_size))
+        .map(|(mut buckets, bucket)| {
+            let mut offsets = [0usize; 256];
+            let mut next_counts = Vec::new();
+            next_counts.resize_with(num_tiles, || [0usize; 256]);
+
+            let chunks = bucket.chunks_exact(4);
+            let rem = chunks.remainder();
+
+            chunks.into_iter().for_each(|chunk| {
+                let b0 = chunk[0].get_level(level) as usize;
+                let nb0 = chunk[0].get_level(next_level) as usize;
+                let b1 = chunk[1].get_level(level) as usize;
+                let nb1 = chunk[1].get_level(next_level) as usize;
+                let b2 = chunk[2].get_level(level) as usize;
+                let nb2 = chunk[2].get_level(next_level) as usize;
+                let b3 = chunk[3].get_level(level) as usize;
+                let nb3 = chunk[3].get_level(next_level) as usize;
+
+                next_counts[(buckets[b0].0 + offsets[b0]) >> tile_exponent][nb0] += 1;
+                buckets[b0].1[offsets[b0]] = chunk[0];
+                offsets[b0] += 1;
+
+                next_counts[(buckets[b1].0 + offsets[b1]) >> tile_exponent][nb1] += 1;
+                buckets[b1].1[offsets[b1]] = chunk[1];
+                offsets[b1] += 1;
+
+                next_counts[(buckets[b2].0 + offsets[b2]) >> tile_exponent][nb2] += 1;
+                buckets[b2].1[offsets[b2]] = chunk[2];
+                offsets[b2] += 1;
+
+                next_counts[(buckets[b3].0 + offsets[b3]) >> tile_exponent][nb3] += 1;
+                buckets[b3].1[offsets[b3]] = chunk[3];
+                offsets[b3] += 1;
+            });
+
+            rem.into_iter().for_each(|v| {
+                let b = v.get_level(level) as usize;
+                let next_b = v.get_level(next_level) as usize;
+                let offs = offsets[b];
+
+                next_counts[(buckets[b].0 + offs) >> tile_exponent][next_b] += 1;
+                buckets[b].1[offs] = *v;
+                offsets[b] += 1;
+            });
+
+            next_counts
+        })
+        .collect();
+
+    let mut out = all_counts.pop().unwrap();
+
+    while let Some(subcounts) = all_counts.pop() {
+        for (i, counts) in subcounts.into_iter().enumerate() {
+            for (ii, c) in counts.iter().enumerate() {
+                out[i][ii] += c;
+            }
+        }
+    }
+
+    counts_to_minor_counts(out)
+}
+
+pub fn write<T>(
+    src_bucket: &mut [T],
+    dst_chunks: Vec<Vec<(usize, &mut [T])>>,
+    level: usize,
+    tile_size: usize,
+)
+where
+    T: RadixKey + Sized + Send + Copy + Sync,
+{
+    dst_chunks
+        .into_par_iter()
+        .zip(src_bucket.par_chunks(tile_size))
         .for_each(|(mut buckets, bucket)| {
             let mut offsets = [0usize; 256];
-            let mut ends = [0usize; 256];
 
-            for (i, b) in buckets.iter().enumerate() {
-                if b.len() == 0 {
-                    continue;
-                }
+            let chunks = bucket.chunks_exact(4);
+            let rem = chunks.remainder();
 
-                ends[i] = b.len() - 1;
-            }
+            chunks.into_iter().for_each(|chunk| {
+                let b0 = chunk[0].get_level(level) as usize;
+                let b1 = chunk[1].get_level(level) as usize;
+                let b2 = chunk[2].get_level(level) as usize;
+                let b3 = chunk[3].get_level(level) as usize;
 
-            let mut left = 0;
-            let mut right = bucket.len() - 1;
-            let pre = bucket.len() % 8;
+                buckets[b0].1[offsets[b0]] = chunk[0];
+                offsets[b0] += 1;
 
-            for _ in 0..pre {
-                let b = bucket[right].get_level(level) as usize;
+                buckets[b1].1[offsets[b1]] = chunk[1];
+                offsets[b1] += 1;
 
-                buckets[b][ends[b]] = bucket[right];
-                ends[b] = ends[b].saturating_sub(1);
-                right = right.saturating_sub(1);
-            }
+                buckets[b2].1[offsets[b2]] = chunk[2];
+                offsets[b2] += 1;
 
-            loop {
-                if left >= right {
-                    break;
-                }
+                buckets[b3].1[offsets[b3]] = chunk[3];
+                offsets[b3] += 1;
+            });
 
-                let bl_0 = bucket[left].get_level(level) as usize;
-                let bl_1 = bucket[left + 1].get_level(level) as usize;
-                let bl_2 = bucket[left + 2].get_level(level) as usize;
-                let bl_3 = bucket[left + 3].get_level(level) as usize;
-                let br_0 = bucket[right].get_level(level) as usize;
-                let br_1 = bucket[right - 1].get_level(level) as usize;
-                let br_2 = bucket[right - 2].get_level(level) as usize;
-                let br_3 = bucket[right - 3].get_level(level) as usize;
+            rem.into_iter().for_each(|v| {
+                let b = v.get_level(level) as usize;
+                let offs = offsets[b];
 
-                buckets[bl_0][offsets[bl_0]] = bucket[left];
-                offsets[bl_0] += 1;
-                buckets[br_0][ends[br_0]] = bucket[right];
-                ends[br_0] = ends[br_0].saturating_sub(1);
-                buckets[bl_1][offsets[bl_1]] = bucket[left + 1];
-                offsets[bl_1] += 1;
-                buckets[br_1][ends[br_1]] = bucket[right - 1];
-                ends[br_1] = ends[br_1].saturating_sub(1);
-                buckets[bl_2][offsets[bl_2]] = bucket[left + 2];
-                offsets[bl_2] += 1;
-                buckets[br_2][ends[br_2]] = bucket[right - 2];
-                ends[br_2] = ends[br_2].saturating_sub(1);
-                buckets[bl_3][offsets[bl_3]] = bucket[left + 3];
-                offsets[bl_3] += 1;
-                buckets[br_3][ends[br_3]] = bucket[right - 3];
-                ends[br_3] = ends[br_3].saturating_sub(1);
-
-                left += 4;
-                right = right.saturating_sub(4);
-            }
+                buckets[b].1[offs] = *v;
+                offsets[b] += 1;
+            });
         });
 }
 
@@ -120,11 +191,16 @@ where
         return;
     }
 
-    let mut tmp_bucket = get_tmp_bucket(bucket.len());
+    let bucket_len = bucket.len();
+    let mut tmp_bucket = get_tmp_bucket(bucket_len);
     let levels: Vec<usize> = (start_level..=end_level).into_iter().collect();
     let mut invert = false;
     let mut first = true;
 
+    let tile_exponent = 23;
+    let tile_size = 1 << tile_exponent;
+    let num_tiles = cdiv(bucket_len, tile_size);
+    let mut next_counts = Vec::new();
 
     for level in levels {
         if first == true && (end_level - start_level) % 2 == 0 {
@@ -136,9 +212,29 @@ where
             ska_sort(bucket, &mut prefix_sums, &end_offsets, level);
         } else {
             if invert {
-                mt_lsb_sort(&mut tmp_bucket, bucket, level);
+                if next_counts.is_empty() {
+                    next_counts = get_minor_counts(tile_size, &mut tmp_bucket, level);
+                }
+
+                let dst_chunks = chunk_and_collate(bucket, next_counts.clone(), num_tiles);
+
+                if level == end_level {
+                    write(&mut tmp_bucket, dst_chunks, level, tile_size);
+                } else {
+                    next_counts = write_and_count(&mut tmp_bucket, dst_chunks, level, tile_exponent, tile_size, num_tiles);
+                }
             } else {
-                mt_lsb_sort(bucket, &mut tmp_bucket, level);
+                if next_counts.is_empty() {
+                    next_counts = get_minor_counts(tile_size, bucket, level);
+                }
+
+                let dst_chunks = chunk_and_collate(&mut tmp_bucket, next_counts.clone(), num_tiles);
+
+                if level == end_level {
+                    write(bucket, dst_chunks, level, tile_size);
+                } else {
+                    next_counts = write_and_count(bucket, dst_chunks, level, tile_exponent, tile_size, num_tiles);
+                }
             };
 
             invert = !invert;
