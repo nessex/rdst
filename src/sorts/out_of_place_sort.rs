c1c6e5dfdcf3e3dcf492408673627c9fba74d9d3
Nathan Essex <nathan@essex.id.au>
2021-11-19 01:01:44 +0900

    Use 2 count arrays in oop sort with counts

--- src/sorts/out_of_place_sort.rs
+++ src/sorts/out_of_place_sort.rs
@@ -58,7 +58,8 @@ where
 {
     let next_level = level + 1;
     let mut prefix_sums = get_prefix_sums(counts);
-    let mut next_counts = [0usize; 256];
+    let mut next_counts_0 = [0usize; 256];
+    let mut next_counts_1 = [0usize; 256];
 
     let chunks = src_bucket.chunks_exact(8);
     let rem = chunks.remainder();
@@ -83,28 +84,28 @@ where
 
         dst_bucket[prefix_sums[b0]] = chunk[0];
         prefix_sums[b0] += 1;
-        next_counts[bn0] += 1;
+        next_counts_0[bn0] += 1;
         dst_bucket[prefix_sums[b1]] = chunk[1];
         prefix_sums[b1] += 1;
-        next_counts[bn1] += 1;
+        next_counts_1[bn1] += 1;
         dst_bucket[prefix_sums[b2]] = chunk[2];
         prefix_sums[b2] += 1;
-        next_counts[bn2] += 1;
+        next_counts_0[bn2] += 1;
         dst_bucket[prefix_sums[b3]] = chunk[3];
         prefix_sums[b3] += 1;
-        next_counts[bn3] += 1;
+        next_counts_1[bn3] += 1;
         dst_bucket[prefix_sums[b4]] = chunk[4];
         prefix_sums[b4] += 1;
-        next_counts[bn4] += 1;
+        next_counts_0[bn4] += 1;
         dst_bucket[prefix_sums[b5]] = chunk[5];
         prefix_sums[b5] += 1;
-        next_counts[bn5] += 1;
+        next_counts_1[bn5] += 1;
         dst_bucket[prefix_sums[b6]] = chunk[6];
         prefix_sums[b6] += 1;
-        next_counts[bn6] += 1;
+        next_counts_0[bn6] += 1;
         dst_bucket[prefix_sums[b7]] = chunk[7];
         prefix_sums[b7] += 1;
-        next_counts[bn7] += 1;
+        next_counts_1[bn7] += 1;
     });
 
     rem.iter().for_each(|val| {
@@ -112,8 +113,12 @@ where
         let bn = val.get_level(next_level) as usize;
         dst_bucket[prefix_sums[b]] = *val;
         prefix_sums[b] += 1;
-        next_counts[bn] += 1;
+        next_counts_0[bn] += 1;
     });
 
-    next_counts
+    for i in 0..256 {
+        next_counts_0[i] += next_counts_1[i];
+    }
+
+    next_counts_0
 }
