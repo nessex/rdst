866a7b21c991e4ccdb850821545850c944b81348
Nathan Essex <nathan@essex.id.au>
2021-11-24 23:34:03 +0900

    Update examples with example of a custom tuner

---
+++ examples/custom_tuner.rs
@@ -0,0 +1,24 @@
+use rdst::RadixSort;
+use rdst::tuner::{Algorithm, Tuner, TuningParams};
+
+struct MyTuner;
+
+impl Tuner for MyTuner {
+    fn pick_algorithm(&self, p: &TuningParams, _counts: &[usize]) -> Algorithm {
+        if p.input_len >= 500_000 {
+            Algorithm::Ska
+        } else {
+            Algorithm::Lsb
+        }
+    }
+}
+
+fn main() {
+    let mut inputs = Vec::new();
+    inputs.extend_from_slice(&[55, 22, 73, 4, 89, 0, 100, 3]);
+
+    inputs.radix_sort_builder()
+        .with_tuner(&MyTuner {})
+        .sort();
+    println!("{:?}", &inputs[..]);
+}
