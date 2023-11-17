d8c89b66988434708cd28dfbcbc1de64eb411e7b
Nathan Essex <nathan@essex.id.au>
2023-11-17 19:56:43 +0900

    Run cargo fmt

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,12 +1,6 @@
-#[cfg(all(
-    feature = "multi-threaded",
-    any(test, bench, tuning)
-))]
+#[cfg(all(feature = "multi-threaded", any(test, bench, tuning)))]
 pub mod bench_utils;
-#[cfg(all(
-    feature = "multi-threaded",
-    any(test, bench, tuning)
-))]
+#[cfg(all(feature = "multi-threaded", any(test, bench, tuning)))]
 pub mod test_utils;
 
 mod sort_utils;
