11d5f5a5e0485609bf96602d23af2a9af04cab78
Nathan Essex <nathan@essex.id.au>
2024-02-03 01:39:07 +0900

    Bump version to v0.20.14

--- src/sorts/mod.rs
+++ src/sorts/mod.rs
@@ -10,16 +10,3 @@ mod regions_sort;
 #[cfg(feature = "multi-threaded")]
 mod scanning_sort;
 mod ska_sort;
-
-pub use comparative_sort::*;
-pub use lsb_sort::*;
-#[cfg(feature = "multi-threaded")]
-pub use mt_lsb_sort::*;
-pub use out_of_place_sort::*;
-#[cfg(feature = "multi-threaded")]
-pub use recombinating_sort::*;
-#[cfg(feature = "multi-threaded")]
-pub use regions_sort::*;
-#[cfg(feature = "multi-threaded")]
-pub use scanning_sort::*;
-pub use ska_sort::*;
